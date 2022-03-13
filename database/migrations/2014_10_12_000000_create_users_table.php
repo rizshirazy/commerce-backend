d5467518838ae7c6e482bc4ba21520e92bb25da1
Muhammad Rizki <hammadri@MacBook-Pro-Muhammad.local>
2022-03-13 14:05:02 +0700

    Add models and migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('roles')->default('USER');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
