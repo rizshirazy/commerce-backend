ad2168b1fff97e2c4b21845901fa715abeea5d7d
Muhammad Rizki <hammadri@MacBook-Pro-Muhammad.local>
2022-03-16 22:20:58 +0700

    Add login end point

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -6,6 +6,7 @@ use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use Laravel\Fortify\Rules\Password;
@@ -43,6 +44,50 @@ class UserController extends Controller
                     'message'    => 'Something went wrong',
                     'error'      => $e,
                     'validation' => $e->errors()
+                ],
+                'Registration Failed',
+                500
+            );
+        }
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email'    => ['required', 'email'],
+                'password' => ['required']
+            ]);
+
+            $credentials = request(['email', 'password']);
+
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'
+                ], 'Authentication Failed', 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!Hash::check($request->password, $user->password)) {
+                throw new \Exception("Invalid Credentials");
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success(
+                [
+                    'access_token' => $tokenResult,
+                    'token_type'   => 'Bearer',
+                    'user'         => $user
+                ],
+                'Login success.'
+            );
+        } catch (\Exception $e) {
+            return ResponseFormatter::error(
+                [
+                    'message'    => 'Something went wrong',
+                    'error'      => $e,
                 ],
                 'Authentication Failed',
                 500
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
 
 Route::get('products', [ProductController::class, 'all']);
 Route::get('categories', [ProductCategoryController::class, 'all']);
